b50ff13ca8b2afe6db453fcfdd70b7daaf9a7031
khanhxoan <khanhxoan99@gmail.com>
2025-08-27 14:42:45 +0700

    Update file release

--- scripts/release.js
+++ scripts/release.js
@@ -1,7 +1,6 @@
 import exec from 'exec-sh';
 import fs from 'fs';
 import path from 'path';
-import { rimraf } from 'rimraf';
 import * as url from 'url';
 
 const pkg = JSON.parse(fs.readFileSync(new URL('../package.json', import.meta.url)));
@@ -36,16 +35,6 @@ async function release() {
     `${JSON.stringify(childPkg, null, 2)}\n`,
   );
 
-  await exec.promise('git pull');
-  await exec.promise('npm i');
-  rimraf.sync(path.resolve(__dirname, 'dir'));
-  fs.mkdirSync(path.resolve(__dirname, 'dir'));
-  await exec.promise(`npm run build:prod`);
-  await exec.promise('git add .');
-  await exec.promise(`git commit -m ${pkg.version} --no-verify`);
-  await exec.promise('git push');
-  await exec.promise(`git tag v${pkg.version}`);
-  await exec.promise('git push origin --tags');
   if (options.beta) {
     await exec.promise('cd ./dist && npm publish --tag beta');
   } else if (options.alpha || options.next) {
